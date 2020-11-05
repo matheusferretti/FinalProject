9e3621647fa6b077c5bad66c9ca42157ec1c9262
Matheus Monteiro <matheusferretti96@gmail.com>
2020-11-05 21:22:43 +0000

    updating

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,14 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">WELCOME TO S/W</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary">LOG-IN</button>
+				</Link>
+				<Link to="/demo">
+					<button className="btn btn-secondary ml-auto">SIGN-UP</button>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,31 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+// import logoDraft from "../../img/LogoDraft.JPG";
 import "../../styles/home.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<h1>SYMP-WEB</h1>
+		<div className="d-flex justify-content-center">
+			<form className="form-inline my-2 my-lg-4">
+				<input
+					className="form-control mr-sm-2"
+					type="search"
+					placeholder="Search for jobs, companies, or prospects"
+					aria-label="Search"
+				/>
+				<button className="btn btn-outline-primary my-2 my-sm-0" type="submit">
+					Search
+				</button>
+			</form>
+		</div>
+		<div className="text-left mt-5 ml-5">
+			<h1>ABOUT US</h1>
+		</div>
+		<div className="text-right mt-5 mr-5">
+			<h1>WHY SYMP-WEB?</h1>
+		</div>
+		<div className="text-left mt-5 ml-5">
+			<h1>CONTACT US!</h1>
+		</div>
 	</div>
 );
