efacec73490946bdf1c6bd49d3ebfb467317dbae
Matheus Monteiro <matheusferretti96@gmail.com>
2020-11-10 00:23:23 +0000

    updating

--- src/js/views/recruiter.js
+++ src/js/views/recruiter.js
@@ -4,7 +4,36 @@ import "../../styles/home.scss";
 export const Recruiter = () => {
 	return (
 		<div>
-			<h1>Recruiter</h1>
+			<div className="card float-left m-5" style={{ width: "18rem" }}>
+				<img src="" className="rounded-circle" alt="" />
+				<div className="card-body">
+					<h5 className="card-title">SYMP-WEB</h5>
+					<p className="card-text">Company Type (Tech, HR)</p>
+				</div>
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">Ratings</li>
+					<li className="list-group-item">Website Link</li>
+					<li className="list-group-item">Social Media Link</li>
+				</ul>
+			</div>
+			<div className="container">
+				<div className="jumbotron jumbotron-fluid">
+					<div className="container">
+						<h1 className="display-5">Introduction</h1>
+						<p className="lead">Description</p>
+					</div>
+				</div>
+				<div className="jumbotron jumbotron-fluid">
+					<div className="container">
+						<h1 className="display-4">Jobs Posted</h1>
+						<ul>
+							<li>One</li>
+							<li>Two</li>
+							<li>Three</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/js/views/user.js
+++ src/js/views/user.js
@@ -4,7 +4,54 @@ import "../../styles/home.scss";
 export const User = () => {
 	return (
 		<div>
-			<h1>Hello!</h1>
+			<div className="card float-left m-5" style={{ width: "18rem" }}>
+				<img src="" className="rounded-circle" alt="" />
+				<div className="card-body">
+					<h5 className="card-title">Matheus Ferretti</h5>
+					<p className="card-text">Python Developer</p>
+				</div>
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">Education</li>
+					<li className="list-group-item">Ratings</li>
+					<li className="list-group-item">
+						Skills
+						<ul>
+							<li>Skill 1</li>
+							<li>Skill 2</li>
+							<li>Skill 3</li>
+						</ul>
+					</li>
+					<li className="list-group-item">Availability</li>
+				</ul>
+			</div>
+			<div className="container">
+				<div className="jumbotron jumbotron-fluid">
+					<div className="container">
+						<h1 className="display-5">Introduction</h1>
+						<p className="lead">Description</p>
+					</div>
+				</div>
+				<div className="jumbotron jumbotron-fluid">
+					<div className="container">
+						<h1 className="display-4">My Projects</h1>
+						<ul>
+							<li>One</li>
+							<li>Two</li>
+							<li>Three</li>
+						</ul>
+					</div>
+				</div>
+				<div className="jumbotron jumbotron-fluid">
+					<div className="container">
+						<h1 className="display-4">Experience</h1>
+						<ul>
+							<li>One</li>
+							<li>Two</li>
+							<li>Three</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
