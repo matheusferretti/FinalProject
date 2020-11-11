937e74ce0a4f4d258545566939e4d66242ff7716
Matheus Monteiro <matheusferretti96@gmail.com>
2020-11-11 00:08:46 +0000

    updating

--- src/js/views/profile.js
+++ src/js/views/profile.js
@@ -7,5 +7,5 @@ import "../../styles/home.scss";
 export const Profile = () => {
 	const { store, actions } = useContext(Context);
 
-	return <div>{store.profile[0].type == "user" ? <User /> : <Recruiter />}</div>;
+	return <div>{store.profile.type == "user" ? <User /> : <Recruiter />}</div>;
 };
