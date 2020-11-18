001f966090d5f4492d7928a322eeeed591aa5b0d
Matheus Monteiro <matheusferretti96@gmail.com>
2020-11-18 18:22:23 +0000

    updating

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -44,12 +44,6 @@ export const Home = () => (
 			</div>
 		</div>
 		<div className="mt-3">
-			{/* <div className="text-right mt-5 mr-5">
-				<h3>
-					WHY S/W?
-					<img src={person2} />
-				</h3>
-			</div> */}
 			<div className="jumbo jumbotron mr-auto">
 				<div className="container">
 					<h1 className="display-4">WHY S/W?</h1>
@@ -71,44 +65,4 @@ export const Home = () => (
 			</h3>
 		</div>
 	</div>
-
-	// <div className="text-center mt-5">
-	// 	<img src={logoDraft} />
-	// 	<div className="d-flex justify-content-center">
-	// 		<form className="form-inline my-2 my-lg-4">
-	// 			<input
-	// 				className="form-control mr-sm-2"
-	// 				type="search"
-	// 				placeholder="Search for jobs, companies, or prospects"
-	// 				aria-label="Search"
-	// 			/>
-	// 			<button className="btn btn-outline-primary my-2 my-sm-0" type="submit">
-	// 				Search
-	// 			</button>
-	// 		</form>
-	// 	</div>
-	// 	<div className="d-flex">
-	// 	<div className="d-flex text-left d-inline">
-	// 		<h3 className="mt-5 ml-5 animate__backInLeft">
-	// 			<img src={person1} />
-	// 			ABOUT US
-	// 		</h3>
-	// 	</div>
-	// 	<div className="d-flex align-items-center">
-	// 		<h2 className="text-right">We are a company that...</h2>
-	// 	</div>
-	// 	</div>
-	// 	<div className="text-right mt-5 mr-5">
-	// 		<h3>
-	// 			WHY S/W?
-	// 			<img src={person2} />
-	// 		</h3>
-	// 	</div>
-	// 	<div className="text-left mt-5 ml-5">
-	// 		<h3>
-	// 			<img src={person3} />
-	// 			CONTACT US!
-	// 		</h3>
-	// 	</div>
-	// </div>
 );
