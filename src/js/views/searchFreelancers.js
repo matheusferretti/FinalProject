a7067a49c76205bcf617478a36f1d23d1617943b
Matheus Monteiro <matheusferretti96@gmail.com>
2020-11-24 00:14:09 +0000

    updating

--- src/js/views/searchFreelancers.js
+++ src/js/views/searchFreelancers.js
@@ -46,64 +46,6 @@ export const SearchFreelancers = () => {
 						})}
 					</div>
 				</div>
-				{/* <div className="jumbotron jumbotron-fluid">
-					<div className="container">
-						<h1 className="display-4">Jobs Posted</h1>
-						<div className="d-flex">
-							<div className="card m-3" style={{ width: "18rem" }}>
-								<img
-									src={gitHub}
-									className="rounded m-4"
-									style={{ width: "50px", height: "50px" }}
-									alt=""
-								/>
-								<div className="card-body">
-									<h5 className="card-title">Security Analyst - GitHub Security Lab</h5>
-									<p className="card-text">GitHub</p>
-									<p className="card-text">Miami, FL</p>
-									<button className="btn btn-outline-primary my-2 my-sm-0" type="submit">
-										Apply
-									</button>
-								</div>
-								<small className="card-text text-muted m-3">Posted 5 days ago</small>
-							</div>
-							<div className="card m-3" style={{ width: "18rem" }}>
-								<img
-									src={gitHub}
-									className="rounded m-4"
-									style={{ width: "50px", height: "50px" }}
-									alt=""
-								/>
-								<div className="card-body">
-									<h5 className="card-title">Data Scientist - Business Intelligence</h5>
-									<p className="card-text">GitHub</p>
-									<p className="card-text">Orlando, FL</p>
-									<button className="btn btn-outline-primary my-2 my-sm-0" type="submit">
-										Apply
-									</button>
-								</div>
-								<small className="card-text text-muted m-3">Posted 1 month ago</small>
-							</div>
-							<div className="card m-3" style={{ width: "18rem" }}>
-								<img
-									src={gitHub}
-									className="rounded m-4"
-									style={{ width: "50px", height: "50px" }}
-									alt=""
-								/>
-								<div className="card-body">
-									<h5 className="card-title">Software Engineer - Admin Experiences</h5>
-									<p className="card-text">GitHub</p>
-									<p className="card-text">Chatanooga, TN</p>
-									<button className="btn btn-outline-primary my-2 my-sm-0" type="submit">
-										Apply
-									</button>
-								</div>
-								<small className="card-text text-muted m-3">Posted 2 weeks ago</small>
-							</div>
-						</div>
-					</div>
-				</div> */}
 			</div>
 		</div>
 	);
